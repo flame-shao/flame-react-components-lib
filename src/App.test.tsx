6afdbae16cf47acbaaacb3824ac2a77196a9387b
flame.shao <shaochong19891230@gmail.com>
2023-12-27 14:36:03 +0800

    [chore]: export as a lib

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
-import App from "./App";
-
-test("root App", () => {
-  render(<App />);
-  const AppElement = screen.getByTestId("root-element");
-  expect(AppElement).toBeInTheDocument();
-});
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 import "./styles/index.scss";
-import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import { library } from "@fortawesome/fontawesome-svg-core";
@@ -9,14 +6,10 @@ import { fas } from "@fortawesome/free-solid-svg-icons";
 
 library.add(fas as any);
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export { default as Button } from "./components/Button";
+export { default as Menu } from "./components/Menu";
+export { default as Icon } from "./components/Icon";
+export { default as Transition } from "./components/Transition";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
